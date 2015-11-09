02fe98e93c311c1b87860b38b34933c0272d2fe8
Gilderien <shadowolf746@gmail.com>
2015-11-08 20:48:17 -0600

    Final Data
    These will be the final pushes a little over an hour before we're done.
    With edits by paul clarke we got a final working data set.

--- crimesorter.py
+++ crimesorter.py
@@ -2,7 +2,7 @@ import csv
 import math
 
 
-def gen_data(filename1, filename2):
+def read_file(filename):
     types_of_crime = {
         'homicide': [0, 0, 205],
         'crim sexual assault': [0, 0, 115],
@@ -28,48 +28,97 @@ def gen_data(filename1, filename2):
         'narcotics': [0, 0, 17.1],
         'liquor law violation': [0, 0, 5.1],
         'gambling': [0, 0, 5],
-        'prostitution': [0, 0, 15]}
-    with open(filename1, 'r') as f:
+        'prostitution': [0, 0, 15],
+        'other narcotic violation': [0, 0, 17.1],
+        'concealed carry license violation': [0, 0, 49.5]}
+
+    with open(filename, 'r') as f:
         reader = csv.reader(f, delimiter=',')
         for row in reader:
-            for category in types_of_crime:
-                if row[6] == 'N':
-                    if row[2].lower() == category:
-                        types_of_crime[category][0] += 1
-                if row[5] == "Y":
-                    if row[2].lower() == category:
-                        types_of_crime[category][1] += 1
+            print row[5].lower()
+            if row[5].lower() == 'n':
+                types_of_crime[row[1].lower()][0] += 1
+                if row[4].lower() == 'y':
+                    types_of_crime[row[1].lower()][1] += 1
 
-    with open(filename2, 'w+') as f:
+    return types_of_crime
+
+
+def process(types_of_crime):
+    out_dict = {}
+    for d in types_of_crime:
+        if types_of_crime[d][0] > 0:
+            out_dict[d] = [types_of_crime[d][0], types_of_crime[d][1], types_of_crime[d][0] * types_of_crime[d][2] * (
+                                        1 / (math.pow(math.e, 2 * types_of_crime[d][1] / types_of_crime[d][0])))]
+        else:
+            out_dict[d] = [types_of_crime[d][0], types_of_crime[d][1], 0]
+
+    return out_dict
+
+
+def write_file(out_dict, filename):
+    with open(filename, 'w+') as f:
         writer = csv.writer(f, delimiter=',')
-        for d in types_of_crime:
-            if types_of_crime[d][0] > 0:
-                writer.writerow([d, types_of_crime[d][0], types_of_crime[d][1],
-                                types_of_crime[d][0] * types_of_crime[d][2] * (
-                                1 / (math.pow(math.e, 2 * types_of_crime[d][1] / types_of_crime[d][0])))])
-            else:
-                writer.writerow([d, types_of_crime[d][0], types_of_crime[d][1],
-                                0])
+        for d in out_dict:
+            writer.writerow([d] + out_dict[d])
 
-gen_data('District1.csv', 'Data1.csv')
-gen_data('District2.csv', 'Data2.csv')
-gen_data('District3.csv', 'Data3.csv')
-gen_data('District4.csv', 'Data4.csv')
-gen_data('District5.csv', 'Data5.csv')
-gen_data('District6.csv', 'Data6.csv')
-gen_data('District7.csv', 'Data7.csv')
-gen_data('District8.csv', 'Data8.csv')
-gen_data('District9.csv', 'Data9.csv')
-gen_data('District10.csv', 'Data10.csv')
-gen_data('District11.csv', 'Data11.csv')
-gen_data('District12.csv', 'Data12.csv')
-gen_data('District14.csv', 'Data14.csv')
-gen_data('District15.csv', 'Data15.csv')
-gen_data('District16.csv', 'Data16.csv')
-gen_data('District17.csv', 'Data17.csv')
-gen_data('District18.csv', 'Data18.csv')
-gen_data('District19.csv', 'Data19.csv')
-gen_data('District20.csv', 'Data20.csv')
-gen_data('District22.csv', 'Data22.csv')
-gen_data('District24.csv', 'Data24.csv')
-gen_data('District25.csv', 'Data25.csv')
+
+def gen_data(infile, outfile):
+    types_of_crime = read_file(infile)
+    reports = 0
+    arrests = 0
+    print types_of_crime
+    for d in types_of_crime:
+        reports += types_of_crime[d][0]
+        arrests += types_of_crime[d][1]
+    if reports > 0:
+        print arrests / reports, reports
+    out_dict = process(types_of_crime)
+    write_file(out_dict, outfile)
+
+
+def gen_for_all():
+    for district in [t for t in range(1, 26) if t not in [13, 21, 23]]:
+        gen_data('District' + str(district) + '.csv', 'Data' + str(district) + '.csv')
+
+
+def gen_one(outfile):
+    types_l = {}
+    for district in [t for t in range(1, 26) if t not in [13, 21, 23]]:
+        types_l[district] = (process(read_file('District' + str(district) + '.csv')))
+
+    rows = []
+
+    for d in types_l[1]:  # crimes
+        row = [d]
+        for district in [t for t in range(1, 26) if t not in [13, 21, 23]]:
+            for i in range(len(types_l[district][d])):
+                row.append(types_l[district][d][i])
+
+        rows.append(row)
+
+    with open(outfile, 'w+') as f:
+        writer = csv.writer(f, delimiter=',')
+        for r in rows:
+            writer.writerow(r)
+
+
+def split():
+    with open('all_crimes.csv', 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        district = 1
+        out = open('District1.csv', 'w+')
+        writer = csv.writer(out, delimiter=',')
+        for row in reader:
+            jayz = int(str(row[6])[:-2])
+            if jayz != district:
+                out.close()
+                district = int(str(row[6])[:-2])
+                if district > 25:
+                    return
+                out = open('District' + str(district) + '.csv', 'w+')
+                writer = csv.writer(out, delimiter=',')
+            writer.writerow(row)
+
+
+gen_one('data.csv')
